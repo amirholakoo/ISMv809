575fd78c63fba054d7ae23263756e8ef25fcc356
amirholakoo <amirholakoo@gmail.com>
2024-01-30 01:20:09 -0800

    Update get_material_types.php

--- get_material_types.php
+++ get_material_types.php
@@ -17,3 +17,26 @@ if (isset($_POST['supplier_id'])) {
 
 $conn->close();
 ?>
+
+<?php
+include 'connect_db.php';
+
+if (isset($_POST['supplier_id'])) {
+    $supplierID = $_POST['supplier_id'];
+
+    $query = "SELECT DISTINCT MaterialType FROM RawMaterials WHERE SupplierID = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $supplierID);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    echo "<option value=''>Select Material Type</option>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<option value='" . $row['MaterialType'] . "'>" . $row['MaterialType'] . "</option>";
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
