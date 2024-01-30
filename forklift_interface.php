c5da536131c4d5b4a8234dfe135c7a0e9f5df12a
amirholakoo <amirholakoo@gmail.com>
2024-01-30 07:17:39 -0800

    Create forklift_interface.php

---
+++ forklift_interface.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+include 'connect_db.php';
+
+// Include the CSS file
+echo "<link rel='stylesheet' href='style.css'>";
+
+// HTML for Forklift Interface Page
+echo "<div class='container'>";
+echo "<h2>Forklift Operations</h2>";
+
+echo "<div class='forklift-tabs'>";
+echo "<a href='unloading_forklift.php'><button class='btn'>Unloading</button></a>";
+echo "<a href='loading_forklift.php'><button class='btn'>Loading</button></a>";
+echo "<a href='using_forklift.php'><button class='btn'>Using</button></a>";
+echo "<a href='moving_forklift.php'><button class='btn'>Moving</button></a>";
+echo "</div>";
+echo "</div>";
+
+echo "</body></html>";
+$conn->close();
+?>
