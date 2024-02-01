eb1c863c1706f371eb415b219c1fe9441e602117
amirholakoo <amirholakoo@gmail.com>
2024-01-31 22:09:12 -0800

    Update fetch_select_type.php

--- fetch_select_type.php
+++ fetch_select_type.php
@@ -1,26 +1,23 @@
+
 <?php
 // fetch_select_type.php
 include 'connect_db.php';
 
-if (isset($_POST['from_location'])) {
-    $fromLocation = $_POST['from_location'];
+if (isset($_POST['from_anbar'])) {
+    $fromAnbar = $_POST['from_anbar'];
 
-    if ($fromLocation == 'Products') {
-        $query = "SELECT DISTINCT Width FROM Products WHERE Status = 'In-stock' ORDER BY Width";
+    if ($fromAnbar == 'Products') {
+        $query = "SELECT DISTINCT Width FROM Products WHERE Status = 'In-stock'";
+        $result = $conn->query($query);
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value='Width-".$row['Width']."'>Width: ".$row['Width']."</option>";
+        }
     } else {
-        $query = "SELECT DISTINCT MaterialName FROM $fromLocation WHERE Status = 'In-stock' ORDER BY MaterialName";
-    }
-
-    $result = $conn->query($query);
-    if ($fromLocation == 'Products') {
-        echo "<option value='Width'>Select Width</option>";
-    } else {
-        echo "<option value='MaterialName'>Select Material Name</option>";
-    }
-
-    while ($row = $result->fetch_assoc()) {
-        $value = ($fromLocation == 'Products') ? $row['Width'] : $row['MaterialName'];
-        echo "<option value='" . $value . "'>" . $value . "</option>";
+        $query = "SELECT DISTINCT MaterialName FROM $fromAnbar WHERE Status = 'In-stock'";
+        $result = $conn->query($query);
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value='Material-".$row['MaterialName']."'>".$row['MaterialName']."</option>";
+        }
     }
 }
 ?>
