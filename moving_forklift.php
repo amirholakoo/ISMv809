2e0c018613ad9d42e7f7906914bcb46a9d7be834
amirholakoo <amirholakoo@gmail.com>
2024-01-31 21:44:53 -0800

    Create moving_forklift.php

---
+++ moving_forklift.php
@@ -0,0 +1,59 @@
+<?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+include 'connect_db.php';
+
+// Include the CSS file
+echo "<link rel='stylesheet' href='style.css'>";
+
+// List of Anbars and Products for Dropdown
+$anbars = [
+    'Products', 'Anbar_Sangin', 'Anbar_Salon_Tolid', 'Anbar_Parvandeh', 
+    'Anbar_Koochak', 'Anbar_Khamir_Ghadim', 'Anbar_Khamir_Kordan', 
+    'Anbar_Muhvateh_Kardan', 'Anbar_Akhal'
+];
+
+// Handle Material Movement
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['move_material'])) {
+    // ... [Code to handle material movement]
+}
+
+// HTML Form for Moving Material
+echo "<div class='container'>";
+echo "<form method='post' id='move_material_form'>";
+echo "<h2>Move Material</h2>";
+
+echo "From: <select name='from_location' id='from_location'>";
+foreach ($anbars as $anbar) {
+    echo "<option value='$anbar'>$anbar</option>";
+}
+echo "</select> <br>";
+
+echo "Select Type: <select name='select_type' id='select_type'>";
+// Dynamically populate based on selected 'From' location
+echo "</select> <br>";
+
+echo "Select Item: <div id='select_item'>";
+// Dynamically populate based on selected 'Select Type'
+echo "</div> <br>";
+
+echo "To: <select name='to_location' id='to_location'>";
+foreach ($anbars as $anbar) {
+    echo "<option value='$anbar'>$anbar</option>";
+}
+echo "</select> <br>";
+
+echo "<input type='submit' name='move_material' value='Move'>";
+echo "</form>";
+echo "</div>";
+
+// JavaScript for dynamic dropdowns and form processing
+echo "<script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>";
+echo "<script>
+    // JavaScript to dynamically load 'Select Type' and 'Select Item' based on selected 'From' location
+    // Add AJAX code here
+</script>";
+
+echo "</body></html>";
+$conn->close();
+?>
